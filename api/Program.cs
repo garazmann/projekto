06913fff54d2c0b46b5b51b148170c88a1771c00
tadypavel <tadypavel@gmail.com>
2025-04-22 23:06:48 +0200

    repository patterns

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,8 @@
 using api.Data;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
+using api.Interfaces;
+using api.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +14,8 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<ApplicationDBContext>(options => {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IStockRepository, StockRepository>();
 
 var app = builder.Build();
 
