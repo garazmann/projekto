05af542c7c6ed8accf37cfa61bbdaf9cbe6a04af
tadypavel <tadypavel@gmail.com>
2025-04-30 22:08:46 +0200

    portfolio delete

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbe39596f88aeb3e421607ccff908c64fdf1308b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5111096add67cc5053e7216e51c863addfa878a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
